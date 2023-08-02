b79ae2fdf08cb1e7d4de98ea887c62c2a2bef11b
= <=>
2023-08-02 09:12:16 +0100

    Added CORS

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
